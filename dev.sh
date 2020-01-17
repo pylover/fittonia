b0cba619275042c25bf464e22c69fe14b73e1c02
pylover <vahid.mardani@gmail.com>
2020-01-17 18:41:00 +0330

    Nothing

--- dev.sh
+++
@@ -1,3 +0,0 @@
-#! /usr/bin/env bash
-
-gunicorn --timeout 9999 --reload fittonia:app
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,15 @@
-#import pytest
+import pytest
 
-#from yhttp import Application
+import fittonia
 
 
-#@pytest.fixture
-#def app():
-#    app = Application()
+@pytest.fixture
+def app():
+    dbname = 'fittonia_test'
+    fittonia.app.settings.merge(f'''
+    db:
+      url: postgres://postgres:postgres@localhost/{dbname}
+    ''')
 
 
 
